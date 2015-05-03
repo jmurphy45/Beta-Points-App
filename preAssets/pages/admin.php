74674ee7eca1a1034c13b6e3ef59f66e5fbc4bec
Administrator <jmrphyii@memphis.edu>
2015-05-03 12:14:02 -0500

    Created basic format of the php admin class

---
+++ preAssets/pages/admin.php
@@ -0,0 +1,44 @@
+<?php
+	class admin{
+		function __construct() {
+			print "In BaseClass constructor\n";
+		}
+
+		function connect(){
+
+
+		}
+
+		function upcomming_events(){
+
+
+		}
+
+		function new_users(){
+
+
+		}
+
+		function total_points(){
+
+
+		}
+
+		function display_users(){
+
+
+		}
+
+		function admin_menu(){
+
+
+		}
+
+		function search(){
+
+			
+		}
+
+	}
+
+?>
